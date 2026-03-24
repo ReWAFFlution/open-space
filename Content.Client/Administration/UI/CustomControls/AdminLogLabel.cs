d7315ee0958bdc6727c1d83de90ac0a11cf4e417
ReWAFFlution <153686236+ReWAFFlution@users.noreply.github.com>
2026-03-24 09:53:58 +0200

    актумор

--- Content.Client/Administration/UI/CustomControls/AdminLogLabel.cs
+++ Content.Client/Administration/UI/CustomControls/AdminLogLabel.cs
@@ -1,17 +1,28 @@
 ﻿using Content.Shared.Administration.Logs;
+using Content.Shared.Database; // OpenSpace-Edit
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Utility; // OpenSpace-Edit
+using System.Text.RegularExpressions; // OpenSpace-Edit
 
 namespace Content.Client.Administration.UI.CustomControls;
 
 public sealed class AdminLogLabel : RichTextLabel
 {
+    private static readonly Regex BracketRegex = new(@"[\[\]]", RegexOptions.Compiled); // OpenSpace-Edit
     public AdminLogLabel(ref SharedAdminLog log, HSeparator separator)
     {
         Log = log;
         Separator = separator;
+        // OpenSpace-Edit Start
+        var impactColor = GetImpactColor(log.Impact);
+        var impactText = $"[color={impactColor}]█[/color]";
 
-        SetMessage($"{log.Date:HH:mm:ss}: {log.Message}");
+        var formatted = new FormattedMessage();
+        formatted.AddMarkupOrThrow($"{impactText} [bold]{log.Date:HH:mm:ss}[/bold]: {log.Message}");
+
+        SetMessage(formatted);
+        // OpenSpace-Edit End
         OnVisibilityChanged += VisibilityChanged;
     }
 
@@ -30,4 +41,14 @@ public sealed class AdminLogLabel : RichTextLabel
 
         OnVisibilityChanged -= VisibilityChanged;
     }
+    // OpenSpace-Edit Start
+    private static string GetImpactColor(LogImpact impact) => impact switch
+    {
+        LogImpact.Extreme => "red",
+        LogImpact.High => "orange",
+        LogImpact.Medium => "yellow",
+        LogImpact.Low => "lightgreen",
+        _ => "gray"
+    };
+    // OpenSpace-Edit End
 }
